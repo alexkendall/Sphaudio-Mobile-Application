9440373b917a5e6ce1df026c9f0ffeba823f24eb
Alexander Harrison <alexkendall.harrison@gmail.com>
2015-11-14 12:53:45 -0500

    Initial Commit

---
+++ SphaudioMobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SphaudioMobile
+//
+//  Created by Alex Harrison on 11/14/15.
+//  Copyright © 2015 Alex Harrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
